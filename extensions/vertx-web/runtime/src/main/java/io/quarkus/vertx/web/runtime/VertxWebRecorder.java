c85a1c3c2fcb9ed7121aa8256ca0b44689d21ac9
Matej Vasek <mvasek@redhat.com>
2020-04-16 15:55:07 +0200

    fixed imports

--- extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/runtime/VertxWebRecorder.java
+++ extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/runtime/VertxWebRecorder.java
@@ -3,7 +3,6 @@ package io.quarkus.vertx.web.runtime;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
